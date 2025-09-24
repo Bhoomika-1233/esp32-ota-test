2169e1752b6b4dcca0fa40129317968afa8172a1
Bhoomika-1233 <bhoomikananjundegowda@gmail.com>
2025-09-24 14:31:52 +0530

    Update otainternet.ino

--- otainternet.ino
+++ otainternet.ino
@@ -26,7 +26,7 @@ const char* otaUpdateUrl = "https://raw.githubusercontent.com/Bhoomika-1233/esp3
 const char* otaVersionUrl = "https://raw.githubusercontent.com/Bhoomika-1233/esp32-ota-test/refs/heads/main/otainternet.ino";  // Replace with your version check URL
 String currentVersion = "1.0.1";
 unsigned long lastOTACheck = 0;
-const unsigned long OTA_CHECK_INTERVAL = 3600000; // Check every hour
+const unsigned long OTA_CHECK_INTERVAL = 60000; // Check every hour
 
 // ------------------ LOGS ------------------
 String logBuffer = "";
